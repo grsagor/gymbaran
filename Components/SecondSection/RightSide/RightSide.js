04a86d9eddd353b79909626b700a6b94f82a65d8
grsagor <grsagor08@gmail.com>
2023-02-04 05:23:35 +0600

    sharp installed

--- Components/SecondSection/RightSide/RightSide.js
+++ Components/SecondSection/RightSide/RightSide.js
@@ -1,15 +1,23 @@
 import Image from 'next/image';
 import right from './right.jpg'
+import background1 from './background1.png'
 
 const RightSide = () => {
     return (
         <div className="w-3/4 mx-auto">
-            <div>
+            <div className='relative'>
+                <div className='relative z-50' style={{zIndex: 3}}>
                 <Image
                     src={right}
                     width="358px"
                     height="580px"
                 />
+                </div>
+                <div className='absolute -left-10 bottom-0 z-0' style={{zIndex: 1}}>
+                <Image
+                    src={background1}
+                />
+                </div>
             </div>
         </div>
     );
