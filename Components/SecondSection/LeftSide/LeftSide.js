8b12046bb87d898248e8fecfb441be2a638b8659
grsagor <grsagor08@gmail.com>
2023-02-07 22:46:56 +0600

    problem solved

--- Components/SecondSection/LeftSide/LeftSide.js
+++ Components/SecondSection/LeftSide/LeftSide.js
@@ -5,7 +5,7 @@ import Brands from "./Brands/Brands";
 
 const LeftSide = () => {
     return (
-        <div className="w-10/12 lg:w-3/4 mx-auto flex flex-col justify-center items-center">
+        <div className="w-11/12 lg:w-3/4 mx-auto flex flex-col justify-center items-center">
             <div className="mt-20">
             <p className="text-3xl lg:text-5xl mb-10 font-grotesque font-bold">Healthy in side <span className="text-indigo-400">fresh</span> out side</p>
             <p className="text-xs lg:text-base mb-6 font-poppins text-justify text-slate-400">Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine.</p>
