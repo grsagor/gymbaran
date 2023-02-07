23599cc5c701236cb3ccf19f1dc77cf12e358670
grsagor <grsagor08@gmail.com>
2023-02-07 06:17:47 +0600

    positioning done for the upper picture

--- Components/ThirdSection/ThirdSection.js
+++ Components/ThirdSection/ThirdSection.js
@@ -1,10 +1,10 @@
 const ThirdSection = () => {
     return (
         <div className="grid grid-cols-12 mt-10 items-center">
-            <div className="col-span-5 text-4xl">
+            <div className="col-span-12 lg:col-span-5 text-4xl">
                 <p className="px-20 font-grotesque">Healthy in side fresh out side</p>
             </div>
-            <div className="col-span-7 w-3/4 text-slate-400">
+            <div className="col-span-12 lg:col-span-7 w-full lg:w-3/4 text-slate-400">
                 <p>Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine. Even at home we still have to be diligent in exercising, either alone or with your friends at home. Healthy life makes you more excited to live the day</p>
             </div>
         </div>
